7a8aa8e55780d73bcc512767b27a3a4a5882e5da
Flittermischief <kmc113@duke.edu>
2018-12-08 18:45:26 -0500

    idk

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -186,16 +186,11 @@ public class HuffProcessor {
 				throw new HuffException("bad input, no PSEUDO_EOF");
 			} 
 			else {
-				if (bits == 0) {
-					current = current.myLeft;
-				} 
-				else {
-					current = current.myRight;
-				}
+				if (bits == 0) {current = current.myLeft;} 
+				else {current = current.myRight;}
+				
 				if (bits == 1) { // ERROR?  Deleted:  || current.myValue == PSEUDO_EOF
-					if (current.myValue == PSEUDO_EOF) {
-						break;
-					} 
+					if (current.myValue == PSEUDO_EOF) break; 
 					else {
 						out.writeBits(BITS_PER_WORD, current.myValue);
 						current = root;
